eac9b0b2976103bbdf472afee9b33335dae7e1ae
LeeKiWoo <pain0121@naver.com>
2025-11-30 23:19:38 +0900

    chore: use variables

---
+++ playground/basic/main.tf
@@ -0,0 +1,13 @@
+# local_file : provider, resource type
+# hello: resource name
+resource "local_file" "hello" {
+	filename = var.filename
+	content = var.content
+	file_permission = "0700"
+}
+
+resource "random_string" "random_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
---
+++ playground/basic/variable.tf
@@ -0,0 +1,9 @@
+variable "filename" {
+    default = "/tmp/hello.txt"
+    type = string
+    description = "filename"
+}
+
+variable "content" {
+    default = "hello world"
+}
