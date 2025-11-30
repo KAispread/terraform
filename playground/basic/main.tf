1034c1ea36fb5c19dfed84eff8354761083b6d72
LeeKiWoo <pain0121@naver.com>
2025-11-30 23:57:04 +0900

    chore: attributes & output

--- playground/basic/main.tf
+++ playground/basic/main.tf
@@ -2,8 +2,9 @@
 # hello: resource name
 resource "local_file" "hello" {
 	filename = var.filename
-	content = var.content
+	content = "${random_string.random_code.id}"
 	file_permission = "0700"
+    depends_on = [ random_string.random_code ]
 }
 
 resource "random_string" "random_code" {
@@ -11,3 +12,12 @@ resource "random_string" "random_code" {
   special = false
   upper   = false
 }
+
+output random_code_output {
+	value = random_string.random_code.id
+	description = "This value will be the id of random_string.random_code"
+}
+
+# terraform output 
+# terraform output random_code_output 
+# terraform output -json 
